3ca275eb03b7bbff93d6b80062c406c6fa29da33
wokeparis <346781678@qq.com>
2021-07-28 01:10:27 +0800

    change

--- learn_mark/interview/summary_old.cpp
+++ learn_mark/interview/summary_old.cpp
@@ -11,3 +11,118 @@
 //字节对齐问题：字节问题是和系统有关系的，是否和其他东西有关未知，但是一般的情况是：64位系统的指针8字节，32位系统指针4字节。如果没有使用#pragma pack，那么每个成员的起始地址必须和它自身的大小对齐，最后struct的大小要和它内部最大的元素的大小对齐。如果设置了#pragma pack，那么取该成员大小和#pragma pack的最小值作为对齐大小。
 //vector清空的方法，清空 vector<int> a  vector<int>().swap(a);   将a与一个新建的vector交换，只是将大小和首尾指针进行交换，原来的数据变成临时值，会被析构掉。另一种用法： vector<int>(a).swap(a);可以将a变成最适合的大小。
 //1.一个可执行文件 由代码段（text）、只读数据段.rodata（const全局变量）、已初始化的读写数据段data（static，全局变量），未初始化数据区bss(只存储大小和符号等信息，并不事实分配空间)。
+//c++11 final的作用有两个，一个用来标记类不能作为基类，一个用来标记函数无法被重写。 override的作用是标记一定要进行重写，防止出现某些手误造成的错误，而且能更直观。 还有default和delete，delete并不是只能用于那几个默认生成的函数，也能用于转换函数，new重载等
+//构造函数里是可以有this指针的，但是初始化列表里不能有。
+
+
+
+
+
+
+/*
+算法	      是否稳定	是否为原地排序	时间复杂度	   空间复杂度	             备注
+选择排序	    否	          是	    N^2	              1	
+插入排序	    是	          是	   介于N和N^2之间	   1	             取决于输入元素的排列情况
+希尔排序	    否	          是	    NlogN？N^(6⁄5)	  1	
+快速排序	    否	          是	     NlogN	          lgN	             运行效率由概率提供保证
+三向快速排序	否	          是	    介于N和NlogN之间   lgN	         运行效率由概率保证，同时也取决于输入元素的分布情况
+归并排序	    是	          否	      NlogN	          N	
+堆排序	        否	          是	     NlogN	           1	
+*/
+/*
+如何让一个对象只能在堆上创建：  构造函数或析构函数私有或protect
+将类中构造，析构定义为私有或保护，然后定义一个公有的静态成员函数，此种方法，只能使用new创建对象，即只能在堆上创建对象。
+解析：
+当对象建立在栈上时，是由编译器分配内存的，调用构造函数和析构函数，编译器管理了对象的整个周期。如果编译器无法调用析构是怎样的呢？
+如果类的析构函数是私有的，编译器将无法调用析构函数来释放内存。所以，编译器在为类对象分配栈空间时，会先检查析构函数的访问性，不光是析构函数，只要是非静态函数，
+编译器都会检查，如果类的析构函数是私有的，则编译器不会在栈空间上为类对象分配内存。
+
+class AA
+{
+public:
+	AA(){}
+	void destory(){delete this;}//析构的时候不能用delete，因为析构私有  用这个函数
+private:
+	~AA(){}
+};
+
+class A  //这样格式统一些，且能够解决无法继承的问题
+{
+ 
+protected:  
+    A(){}
+    ~A(){}
+    
+public:
+    static A* create()
+    {
+        return new A();
+    }
+    
+    void destory()
+    {  
+        delete this;
+    }
+}
+ 
+int main()
+{
+	//AA q;   //在栈上创建对象
+	AA* p = new AA;   //在堆上创建对象
+	system("pause");
+	return 0;
+}
+
+class AA
+{
+public:
+	~AA(){}
+	static AA* get(){
+		return new AA;
+	}
+	//void destory(){delete this;}//析构的时候不能用delete，因为析构私有  用这个函数
+private:
+	AA(){}
+
+};
+
+int main()
+{
+	AA* a = AA::get();
+	delete a;
+}
+*/
+
+/*
+让一个对象只能在栈上创建：把operator new重载并设置为private或delete,最好delete也做一样的操作，否则不能new 但是能delete，容易发生错误。
+class AA
+{
+private:
+	void* operator new(size_t){}
+	void operator delete(void*){}
+public:
+	AA()
+	{
+		cout << "AA()" << endl;
+	}
+	~AA()
+	{
+		cout << "~AA()" << endl;
+	}
+};
+*/
+
+/*
+epoll水平模式和触发模式
+https://blog.csdn.net/halfclear/article/details/78061771
+https://zhuanlan.zhihu.com/p/63179839
+    下面说的全是触发模式：
+    触发模式的触发条件，以读为例子（写差不多）。 在已经添加的条件下，1.ctl的时候如果里面有值 2.数据从无到有 3.数据值变化。
+    (EPOLLIN|EPOLLOUT)可以这样一次性指定一个文件描述符。读写都检测。  触发读的时候同时会触发是否可写的检测
+    两个epoll监测同一个fd，两个都能收到事件。
+    epoll文件描述符也是可读的。
+    如果读的时候又来数据了，然后读完了，直接触发再来一次recv直接报错也相当于读完。
+    如果读的时候一直来数据。会造成饥饿，所有的其他fd无法操作，然后可以自己维护一个列表，每个fd操作一定次，然后循环操作，直到所有fd都不是active
+    触发条件测试。如果收到一个fin，水平模式和触发模式的情况
+        都会触发，通过判断read返回值=0。也可以通过触发epollrdhup来判断。
+*/
