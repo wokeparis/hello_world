039d7d576c192081f40d32aff00373a623d724bb
wokeparis <346781678@qq.com>
2021-08-10 01:19:35 +0800

    add

--- learn_mark/test.cpp
+++ learn_mark/test.cpp
@@ -1,36 +1,25 @@
+//5本西游记，num = 5，索引条件变量，多线程，
+
+
 #include <iostream>
-#include <stack>
+#include <mutex>
+
 using namespace std;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#include <memory>
+#include <vector>
+ 
+struct A {
+    std::vector<std::unique_ptr<int> > vec;
+};
+ 
+int main(int argc, char* argv[]) {
+    std::vector<A> vec;
+ 
+    A a;
+    a.vec.emplace_back(new int{0});
+ 
+    vec.emplace_back(std::move(a));
+ 
+    return 0;
+}
